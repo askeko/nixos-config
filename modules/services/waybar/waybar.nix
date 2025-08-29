a884b912899b913ed8bbe2fb2d003d71f90e0f97
askeko <askeklitgaard@gmail.com>
2025-08-29 14:08:35 +0200

    100% nix

--- modules/services/waybar/waybar.nix
+++ modules/services/waybar/waybar.nix
@@ -1,96 +1,96 @@
 #
 #  Bar
 #
-
-{ config, lib, pkgs, vars, host, ... }:
-
+{
+  config,
+  lib,
+  vars,
+  host,
+  ...
+}:
 with lib;
-with host; 
-let 
-    theme = builtins.readFile ./styles/onedark.css;
-    style = builtins.readFile ./styles/style.css;
-in {
-  config = mkIf (config.waylandwm.enable) 
+with host; {
+  config =
+    mkIf config.waylandwm.enable
     {
       home-manager.users.${vars.user} = {
         programs.waybar = {
           enable = true;
-          package = pkgs.waybar;
-          style = "${theme}${style}";
+          style = concatStringsSep "\n" [(import ./styles/onedark.nix) (import ./styles/style.nix)];
           settings = {
-          mainBar = {
-            layer = "top"; # Waybar at top layer
-            position = "top"; # Waybar position (top|bottom|left|right)
+            mainBar = {
+              layer = "top"; # Waybar at top layer
+              position = "top"; # Waybar position (top|bottom|left|right)
 
-            # Choose the order of the modules
-            modules-left = ["hyprland/workspaces" "temperature" "cpu" "memory" "disk"];
-            modules-center = ["custom/music"];
-            modules-right = ["custom/pacman" "backlight" "wireplumber" "network" "battery" "clock" "tray"];
+              # Choose the order of the modules
+              modules-left = ["hyprland/workspaces" "temperature" "cpu" "memory" "disk"];
+              modules-center = ["custom/music"];
+              modules-right = ["custom/pacman" "backlight" "wireplumber" "network" "battery" "clock" "tray"];
 
-            "hyprland/workspaces" = {
+              "hyprland/workspaces" = {
                 active-only = false;
                 all-outputs = true;
                 show-special = true;
                 format = "{icon}";
                 format-icons = {
-                    active = "󰣇";
-                    persistent = "";
-                    empty = "";
-                    urgent = "";
-                    default = "󰣇";
+                  active = "󰣇";
+                  persistent = "";
+                  empty = "";
+                  urgent = "";
+                  default = "󰣇";
                 };
                 persistent-workspaces = {
-                    "*" = 10; # 10 workspaces by default on every monitor
+                  "*" = 10; # 10 workspaces by default on every monitor
                 };
-            };
+              };
 
-            tray = {
+              tray = {
                 icon-size = 18;
                 spacing = 10;
-            };
+              };
 
-            temperature = {
+              temperature = {
                 critical-threshold = 80;
                 format-critical = "{icon} {temperatureC}°";
                 format = "{icon} {temperatureC}°";
                 format-icons = ["󱃃" "󱩿" "󰸁"];
-            };
+              };
 
-            cpu = {
+              cpu = {
                 format = "󰻠 {usage}%";
-            };
+              };
 
-            memory = {
+              memory = {
                 format = "󰍛 {}%";
-            };
+              };
 
-            network = {
+              network = {
                 format-wifi = "󰘊 {signalStrength}%";
                 format-ethernet = "󰈀";
                 tooltip-format = "󰈀 {ifname} via {gwaddr}";
                 format-disconnected = "󰞃 Disconnected";
-            };
+              };
 
-            "custom/pacman" = {
+              "custom/pacman" = {
                 format = "{}  ";
-                interval = 1800;                     # every half hour
-                exec = "checkupdates | wc -l";       # num of updates
-                exec-if = "exit 0";                  # always run; consider advanced run conditions
-            };
+                interval = 1800; # every half hour
+                exec = "checkupdates | wc -l"; # num of updates
+                exec-if = "exit 0"; # always run; consider advanced run conditions
+              };
 
-            backlight = {
+              backlight = {
                 format = "{icon}";
                 format-icons = ["󱩎" "󱩏" "󱩐" "󱩑" "󱩒" "󱩓" "󱩔" "󱩕" "󱩖" "󰛨"];
-            };
+              };
 
-            disk = {
+              disk = {
                 interval = 30;
                 format = "󱛟 {used}";
                 unit = "GB";
                 path = "/";
-            };
+              };
 
-            "custom/music" = {
+              "custom/music" = {
                 format = "  {}";
                 escape = true;
                 interval = 5;
@@ -98,40 +98,40 @@ in {
                 exec = "playerctl metadata --format='{{ title }}'";
                 on-click = "playerctl play-pause";
                 max-length = 40;
-            };
+              };
 
-            clock = {
+              clock = {
                 tooltip = false;
                 timezone = "Europe/Copenhagen";
                 format = "󰥔 {:%H:%M | %d/%m}";
-            };
+              };
 
-            battery = {
+              battery = {
                 tooltip = false;
                 full-at = 99;
                 states = {
-                    critical = 15;
+                  critical = 15;
                 };
                 format = "{icon} {capacity}%";
                 format-charging = "󰂄 {capacity}%";
                 format-plugged = "󰚥 {capacity}%";
                 format-icons = ["󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󱐋"];
-            };
+              };
 
-            wireplumber = {
+              wireplumber = {
                 tooltip = false;
                 format = "{icon} {volume}%";
                 format-muted = "";
                 max-volume = 120;
                 format-icons = ["" "" ""];
-            };
+              };
 
-            "custom/scrot" = {
+              "custom/scrot" = {
                 tooltip = false;
                 on-click = "";
                 format = "";
+              };
             };
-          };
           };
         };
       };
