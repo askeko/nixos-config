25255acb96031f2f88a4e0f5bddcc1fd4cdebd27
askeko <askeklitgaard@gmail.com>
2025-09-01 14:53:03 +0200

    restructuring nix setup

--- flake.nix
+++ flake.nix
@@ -8,7 +8,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nvf.url = "github:Bliztle/nvf/emmet-language-server"; # For vim config
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -16,21 +15,89 @@
     };
   };
 
-  outputs = inputs @ {
+  outputs = {
+    self,
     nixpkgs,
     home-manager,
-    nvf,
     ...
-  }: let
-    vars = {
-      user = "askeko";
-      terminal = "kitty";
-      browser = "firefox";
-    };
+  } @ inputs: let
+    inherit (self) outputs;
+    # Supported systems
+    systems = ["x86_64-linux"];
+    # Function
+    forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
-    nixosConfigurations = import ./hosts {
-      inherit (nixpkgs) lib;
-      inherit inputs nixpkgs home-manager nvf vars;
+    # Custom packages, accessible through 'nix-build', 'nix-shell', etc.
+    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+    # Formatter for nix files. Accessible through 'nix fmt'. Use 'alejandra' or 'nixpkgs-fmt'
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+    # Custom packages and modifications, exported as overlays
+    overlays = import ./overlays {inherit inputs;};
+    # Reusable nixos modules you might want to export
+    # These are usually stuff you would upstream into nixpkgs
+    nixosModules = import ./modules/nixos;
+    # Reusable home-manager modules you might want to export
+    # These are usually stuff you would upstream into home-manager
+    homeManagerModules = import ./modules/home-manager;
+
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      # FIXME replace with your hostname
+      yoga7 = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules = [
+          # > Our main nixos configuration file <
+          ./nixos/configuration.nix
+        ];
+      };
+    };
+
+    # Standalone home-manager configuration entrypoint
+    # Available through 'home-manager --flake .#your-username@your-hostname'
+    homeConfigurations = {
+      # FIXME replace with your username@hostname
+      "askeko@absentia" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {inherit inputs outputs;};
+        modules = [
+          # > Our main home-manager configuration file <
+          ./home-manager/home.nix
+        ];
+      };
     };
   };
 }
+# {
+#   description = "Askeko's NixOS Configuration";
+#
+#   inputs = {
+#     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+#     nvf.url = "github:Bliztle/nvf/emmet-language-server"; # For vim config
+#
+#     home-manager = {
+#       url = "github:nix-community/home-manager";
+#       inputs.nixpkgs.follows = "nixpkgs";
+#     };
+#   };
+#
+#   outputs = inputs @ {
+#     self,
+#     nixpkgs,
+#     home-manager,
+#     nvf,
+#     ...
+#   }: let
+#     vars = {
+#       user = "askeko";
+#       terminal = "kitty";
+#       browser = "firefox";
+#     };
+#   in {
+#     nixosConfigurations = import ./hosts {
+#       inherit (nixpkgs) lib;
+#       inherit inputs nixpkgs home-manager nvf vars;
+#     };
+#   };
+# }
+
