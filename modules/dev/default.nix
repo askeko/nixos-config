7a67997ebf24345c68e81f71cd4f678fe6c7aed0
askeko <askeklitgaard@gmail.com>
2025-09-14 13:11:35 +0200

    add haskell

--- modules/dev/default.nix
+++ modules/dev/default.nix
@@ -1,5 +1,18 @@
 {
+  pkgs,
+  vars,
+  ...
+}: {
   imports = [
     ./git.nix
   ];
+  home-manager.users.${vars.user} = {
+    home.packages = with pkgs; [
+      (haskellPackages.ghcWithPackages (pkgs:
+        with pkgs; [
+          stack
+          cabal-install
+        ]))
+    ];
+  };
 }
